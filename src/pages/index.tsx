ad634d294174ba79eb2300e29ae68c3b20e058ab
Mario Škrlec <mario@prototyp.digital>
2020-11-09 19:28:27 +0100

    Project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage: React.FC = () => (
+  <Layout>
+    <SEO title="Home" />
+  </Layout>
+)
+
+export default IndexPage
