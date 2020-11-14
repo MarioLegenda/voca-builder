d35301c48a0fa6c83ee7b5d3a2e63baaaf5d94d7
Mario Škrlec <mario@prototyp.digital>
2020-11-14 14:49:11 +0100

    Removed Email and MinLength constraints since they are not used anywhere

--- src/app/validation/constraints/MinLength.ts
+++
@@ -1,9 +0,0 @@
-import IConstraint, { ConstraintValue } from "./IConstraint"
-
-export default class MinLength implements IConstraint {
-  constructor(private minLength: number) {}
-
-  validate(val: ConstraintValue): string {
-    if (this.minLength > (val as string).length) return 'minLength';
-  }
-}
